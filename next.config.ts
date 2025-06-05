54d1724f96d95b5f129ca61e54e9841d586972b5
techguru0411 <techguru0411@gmail.com>
2025-06-05 22:07:59 +0200

    fix: remove code in next config js

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type,Authorization",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
